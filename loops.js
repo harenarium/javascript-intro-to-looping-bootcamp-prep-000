56417bb13cb0a9b265c00f704ba4006a46d4fe8d
harenarium <carolinel2610@gmail.com>
2017-11-02 23:37:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,7 +6,7 @@ loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."` to an
 
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-  console.log(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+  }
   return array
-}
 }
