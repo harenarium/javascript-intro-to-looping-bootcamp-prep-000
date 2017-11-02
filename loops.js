c073a5fa4219e6f601388c84d17edad97573a189
harenarium <carolinel2610@gmail.com>
2017-11-02 23:40:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,17 @@
 /*
-loops forLoop(array) adds `"I am ${i} strange loop${i === 0 ? '' : 's'}."` to an array 25 times:
   2) loops whileLoop(n) counts down from n to 0:
   3) loops doWhileLoop(array) removes elements from `array` until `array` is empty or until `maybeTrue()` returns `false`:
 */
 
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-  array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
   }
   return array
+}
+
+function whileLoop(n){
+  while(n>=0){
+    console.log(n--)
+  }
 }
